a06b84c86940422cb4d9f6b9524641c6432c3e35
omoanemo1234 <vipada.siripatanadilok@gmail.com>
2023-03-08 20:56:34 +0700

    add job from real json job

--- _resources/bundle_config.py
+++ _resources/bundle_config.py
@@ -1,42 +1,49 @@
 # Databricks notebook source
 {
-  "name": "motest",
-  "category": "Test",
-  "title": "motest_dbdemos.",
-  "description": "Mo test dbdemos 1",
-  "bundle": True,
-  "tags": [],
-  "notebooks": [
-    {
-      "path": "Test_dbdemos_notebook_1.ipynb", 
-      "pre_run": True, 
-      "publish_on_website": True, 
-      "add_cluster_setup_cell": True,
-      "title":  "Mo test 1", 
-      "description": "Mo test dbdemos 1",
-      "parameters": {}
-    }
-  ],
-  "init_job": {
-    "settings": {
-        "name": "demos_mo_init_{{CURRENT_USER_NAME}}",
-        "email_notifications": {
-            "no_alert_for_skipped_runs": False
+    "name": "motest",
+    "category": "Test",
+    "title": "motest_dbdemos.",
+    "description": "Mo test dbdemos 1",
+    "bundle": True,
+    "tags": [],
+    "notebooks": [
+        {
+            "path": "Test_dbdemos_notebook_1.ipynb",
+            "pre_run": True,
+            "publish_on_website": True,
+            "add_cluster_setup_cell": True,
+            "title":  "Mo test 1",
+            "description": "Mo test dbdemos 1",
+            "parameters": {}
+        }
+    ],
+    "init_job": {
+        "job_id": 870724760777759,
+        "creator_user_name": "vipada.siripatanadilok@adastragrp.com",
+        "run_as_user_name": "vipada.siripatanadilok@adastragrp.com",
+        "run_as_owner": True,
+        "settings": {
+            "name": "Task_Test_dbdemos_notebook_1",
+            "email_notifications": {
+                "no_alert_for_skipped_runs": False
+            },
+            "webhook_notifications": {},
+            "timeout_seconds": 0,
+            "max_concurrent_runs": 1,
+            "tasks": [
+                {
+                    "task_key": "Task_Test_dbdemos_notebook_1",
+                    "notebook_task": {
+                        "notebook_path": "/Users/vipada.siripatanadilok@adastragrp.com/dbDemos/Test_dbdemos_notebook_1",
+                        "source": "WORKSPACE"
+                    },
+                    "existing_cluster_id": "0307-065426-vfnmzpu9",
+                    "timeout_seconds": 0,
+                    "email_notifications": {}
+                }
+            ],
+            "format": "MULTI_TASK"
         },
-        "timeout_seconds": 0,
-        "max_concurrent_runs": 1,
-        "tasks": [
-            {
-                "task_key": "init_data",
-                "notebook_task": {
-                    "notebook_path": "{{DEMO_FOLDER}}/_resources/Test_dbdemos_notebook_1",
-                    "source": "WORKSPACE"
-                },
-                "job_cluster_key": "Shared_job_cluster",
-                "timeout_seconds": 0,
-                "email_notifications": {}
-            }
-        ]
+        "created_time": 1678250654220
     }
-}
 }
