bda29315419ac057d6b0efc6160a5148dd36768a
omoanemo1234 <vipada.siripatanadilok@gmail.com>
2023-03-08 20:40:15 +0700

    add job

--- _resources/bundle_config.py
+++ _resources/bundle_config.py
@@ -16,5 +16,26 @@
       "description": "Mo test dbdemos 1",
       "parameters": {}
     }
-  ]
+  ],
+  "init_job": {
+    "settings": {
+        "name": "demos_mo_init_{{CURRENT_USER_NAME}}",
+        "email_notifications": {
+            "no_alert_for_skipped_runs": False
+        },
+        "timeout_seconds": 0,
+        "max_concurrent_runs": 1,
+        "tasks": [
+            {
+                "task_key": "init_data",
+                "notebook_task": {
+                    "notebook_path": "{{DEMO_FOLDER}}/_resources/Test_dbdemos_notebook_1",
+                    "source": "WORKSPACE"
+                },
+                "job_cluster_key": "Shared_job_cluster",
+                "timeout_seconds": 0,
+                "email_notifications": {}
+            }
+        ]
+    }
 }
